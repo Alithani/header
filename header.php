cda1babb453b511326abda8b50a4086f305754d9
Alithani <ascoffy8@gnail.com>
2018-02-15 23:44:14 +0300

    First Message

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <html>
 <head>
-<title><?php echo"My Assignment"; ?></title>
+<title><?php echo"My Assignment OS"; ?></title>
 <link rel="stylesheet" href="stylesheet.css" type="text/css" />
 </head>
 <body>
 <div id="header">
 <h1><?php echo "Header"; ?></h1>
 </div>
-<div id="main">

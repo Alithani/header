16640b64bb1b883e40c9030b6c0e7225530a9fde
Ali <ascoffy8@gmail.com>
2018-02-15 22:53:32 +0300

    initial commit

---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title><?php echo"My Assignment"; ?></title>
+<link rel="stylesheet" href="stylesheet.css" type="text/css" />
+</head>
+<body>
+<div id="header">
+<h1><?php echo "Header"; ?></h1>
+</div>
+<div id="main">
